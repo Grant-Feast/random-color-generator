87fb67ea6cbe5bc0dca71866740b30c3f2628d02
Grant <feast5801@hotmail.co.uk>
2021-02-03 16:40:43 +0000

    make a seperate function for colors

--- colors.js
+++ colors.js
@@ -3,10 +3,19 @@ const btn = document.querySelector('.btn');
 
 // A function which changes the background color when the button is clicked.
 btn.addEventListener('click', function () {
-  const r = Math.floor(Math.random() * 256);
-  const g = Math.floor(Math.random() * 256);
-  const b = Math.floor(Math.random() * 256);
-  const newColor = `rgb(${r}, ${g}, ${b})`;
+  const newColor = randColor()
   document.body.style.backgroundColor = newColor;
   heading.innerText = newColor;
 })
+
+const randColor = () => {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  if (r < 110 && g < 110 && b < 110){
+    heading.style.color = 'white';
+  } else {
+    heading.style.color = 'black';
+  }
+  return `rgb(${r}, ${g}, ${b})`;
+}
